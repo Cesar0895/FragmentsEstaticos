5fc2b0a16dfe380baa9e03888b52db2961269467
Cesar Espino <danger_95@hotmail.es>
2019-08-01 11:38:23 -0600

    Fragment azul y fragment rojo

--- app/src/main/java/com/sunset/fragmentsestaticos/MainActivity.java
+++ app/src/main/java/com/sunset/fragmentsestaticos/MainActivity.java
@@ -1,13 +1,19 @@
 package com.sunset.fragmentsestaticos;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements FragmentRojo.OnFragmentInteractionListener, FragmentAzul.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
